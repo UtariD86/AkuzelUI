6a1f08d757c0868a1377836c2f389b35e9088a21
UtariD86 <69503659+UtariD86@users.noreply.github.com>
2024-05-06 13:45:18 +0300

    ui katmanına tema dahil edildi dashboard'a başlandı

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+
+namespace AkuzelUI.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult GetViewComponent(string component)
+        {
+            
+            return ViewComponent(component/*, new { listingType = listingType, refresh = refresh, Courses = courses }*/);
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AkuzelUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
